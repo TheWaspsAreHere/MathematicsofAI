646e7b5403552a4dd9e7600d46f17526ec8b40d5
TheWaspsAreHere <719045@student.dusd.net>
2026-03-18 11:41:31 -0700

    code
    fimish codingh

---
+++ MoralesK3DMathAIP3/Assets/Move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    float speed = 0.005f;
+
+    void Start() 
+    {
+        direction = goal.transform.position - transform.position;
+        this.transform.position = this.transform.position + direction;
+    }
+
+    private void LateUpdate() 
+    {
+        Vector3 velocity = direction.normalized * speed;
+        this.transform.position = this.transform.position;
+    }
+}
